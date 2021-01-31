add masking aug, fix dataloader

Sleep/dataloader.py
@@ -1,69 +1,107 @@
+from conf import *
+
 import os
 import cv2
+import copy
+import random
 import numpy as np
 from PIL import Image
 import torch
 from torch.utils.data import Dataset, DataLoader 
+from albumentations.pytorch.functional import img_to_tensor
 
 
-def path_loader(root_path):
-    image_path = []
-    image_keys = []
-    for _,_,files in os.walk(os.path.join(root_path,'train_data')):
-        for f in files:
-            path = os.path.join(root_path,'train_data',f)
-            if path.endswith('.png'):
-                image_keys.append(int(f[:-4]))
-                image_path.append(path)
 
-    return np.array(image_keys), np.array(image_path)
-
-
-def label_loader(root_path, keys):
-    labels_dict = {}
-    labels = []
-    with open(os.path.join(root_path, 'train_label'), 'rt') as f :
-        for row in f:
-            row = row.split()
-            labels_dict[int(row[0])] = (int(row[1]))
-    for key in keys:
-        labels = [labels_dict[x] for x in keys]
-    return labels
-
-
-class PathDataset(Dataset): 
-    def __init__(self, image_paths, labels=None, default_transforms=None, transforms=None, is_test=False): 
+class SleepDataset(Dataset): 
+    def __init__(self, image_paths, labels=None, transforms=None, masking='soft', is_test=False): 
         self.image_paths = image_paths
+        self.masking = masking
         self.labels = labels 
-        self.default_transforms = default_transforms
+        # self.default_transforms = default_transforms
         self.transforms = transforms
         self.is_test = is_test
 
-        self.imgs = []
+        # for image_path in self.image_paths:
+            # image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+            # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            # img = Image.fromarray(img)
 
-        for img_path in self.image_paths:
-            img = cv2.imread(img_path)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            img = Image.fromarray(img)
-
-            if self.default_transforms is not None:
-                img = self.default_transforms(img)
-    
-            self.imgs.append(img)
-
-    def __getitem__(self, index):
-        
-        img = self.imgs[index]
-
-        if self.transforms is not None:
-            img = self.transforms(img)
-
-        if self.is_test:
-            return torch.tensor(img, dtype=torch.float32)
-        else:
-            return torch.tensor(img, dtype=torch.float32),\
-                 torch.tensor(self.labels[index], dtype=torch.long)
+            # if self.default_transforms is not None:
+            #     img = self.default_transforms(image=img)['image']
+            # self.images.append(image)
 
     def __len__(self): 
         return len(self.image_paths)
 
+    def __getitem__(self, index):
+        
+        image_path = self.image_paths[index]
+        image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        
+        if self.transforms:
+            image = self.transforms(image=image)['image']
+
+        # Masking Augmentation (Saewon)
+        if self.masking == 'soft':
+            mask_prob = 0.3
+            rand_val = random.random()
+            if rand_val < mask_prob:
+                image = time_mask(image, num_masks=2)
+            rand_val = random.random()
+            if rand_val < mask_prob:
+                image = signal_mask(image, num_masks=2)
+            
+        elif self.masking == 'hard':
+            mask_prob = 0.5
+            rand_val = random.random()
+            if rand_val < mask_prob:
+                image = signal_mask(time_mask(image, num_masks=2), num_masks=2)
+
+        image = img_to_tensor(image, {"mean": [0.485, 0.456, 0.406],
+                                    "std": [0.229, 0.224, 0.225]})
+
+        if self.is_test:
+            return image #torch.tensor(img, dtype=torch.float32)
+        else:
+            label = self.labels[index]
+            return image, label#torch.tensor(img, dtype=torch.float32),\
+                 #torch.tensor(self.labels[index], dtype=torch.long)
+
+
+# Saewon
+# 세로로 masking
+def time_mask(image, T=50, num_masks=1):
+    cloned = copy.deepcopy(image)
+    len_spectro = cloned.shape[1]
+
+    for i in range(0, num_masks):
+        t = random.randrange(10, T)
+        t_zero = random.randrange(10, len_spectro - t)
+
+        # avoids randrange error if values are equal and range is empty
+        if (t_zero == t_zero + t): 
+            return cloned
+
+        mask_end = random.randrange(t_zero, t_zero + t)
+        cloned[:,t_zero:mask_end] = 0
+    
+    return cloned
+
+# 가로로 masking
+def signal_mask(image, S=30, num_masks=1):
+    cloned = copy.deepcopy(image)
+    num_mel_channels = cloned.shape[1]
+    
+    for i in range(0, num_masks):        
+        f = random.randrange(10, S)
+        f_zero = random.randrange(10, num_mel_channels - f)
+        
+        # avoids randrange error if values are equal and range is empty
+        if (f_zero == f_zero + f): 
+            return cloned
+
+        mask_end = random.randrange(f_zero, f_zero + f) 
+        cloned[f_zero:mask_end] = 0
+    
+    return cloned

Sleep/main.py
@@ -1,3 +1,5 @@
+from conf import *
+
 import gc
 import os 
 import argparse
@@ -16,15 +18,15 @@ from torchvision import transforms
 from dataloader import *
 from models import *
 from trainer import *
-from transforms import *
+from transforms import get_transform
 from optimizer import *
-from utils import seed_everything, find_th
+from utils import seed_everything, find_th, LabelSmoothingLoss
 
 import warnings
 warnings.filterwarnings('ignore')
 
 
-def main(args):
+def main():
 
     # fix seed for train reproduction
     seed_everything(args.SEED)
@@ -33,94 +35,65 @@ def main(args):
     print("device", device)
 
 
-    if args.mode == 'train': 
+    # TODO dataset loading
+    # glob setting
+    # image_path = 
+    # labels = 
 
+    # TODO sampling dataset for debugging
+    if args.DEBUG: 
+        total_num = 100
+        # image_path = image_path[:total_num]
+        # labels = labels[:total_num]
 
-        # TODO dataset loading
+    skf = StratifiedKFold(n_splits=args.n_folds, shuffle=True, random_state=args.SEED)
+    for fold_num, (trn_idx, val_idx) in enumerate(skf.split(image_path, labels)):
 
+        print(f"fold {fold_num} training starts...")
+        trn_img_paths = np.array(image_path)[trn_idx]
+        trn_labels = np.array(labels)[trn_idx]
+        val_img_paths = np.array(image_path)[val_idx]
+        val_labels = np.array(labels)[val_idx]
 
-        # TODO sampling dataset for debugging
-        if args.DEBUG: 
-            total_num = 100
-            # image_path = image_path[:total_num]
-            # labels = labels[:total_num]
+        # train_transforms = get_transform(target_size=(args.input_size),
+        #                                 transform_list=args.train_augments)
+        # valid_transforms = get_transform(target_size=(args.input_size),
+        #                                 transform_list=args.train_augments,
+        #                                 is_train=False)
 
-        skf = StratifiedKFold(n_splits=args.n_folds, shuffle=True, random_state=args.SEED)
-        for fold_num, (trn_idx, val_idx) in enumerate(skf.split(image_path, labels)):
+        train_transforms = create_train_transforms(args.input_size)
+        valid_transforms = create_val_transforms(args.input_size)
 
-            print(f"fold {fold_num} training starts...")
-            trn_img_paths = np.array(image_path)[trn_idx]
-            trn_labels = np.array(labels)[trn_idx]
-            val_img_paths = np.array(image_path)[val_idx]
-            val_labels = np.array(labels)[val_idx]
+        train_dataset = SleepDataset(trn_img_paths, trn_labels, train_transforms, masking='soft', is_test=False)
+        valid_dataset = SleepDataset(trn_img_paths, trn_labels, valid_transforms, masking='soft', is_test=False)
 
-            default_transforms = transforms.Compose([transforms.Resize(args.input_size)])
-            train_transforms = get_transform(target_size=(args.input_size, args.input_size),
-                                            transform_list=args.train_augments, 
-                                            augment_ratio=args.augment_ratio)
-                                    
-            valid_transforms = get_transform(target_size=(args.input_size, args.input_size),
-                                            transform_list=args.valid_augments, 
-                                            augment_ratio=args.augment_ratio,
-                                            is_train=False)  
+        train_loader = DataLoader(dataset=train_dataset, batch_size=args.batch_size, num_workers=args.num_workers, shuffle=True, pin_memory=True)
+        valid_loader = DataLoader(dataset=valid_dataset, batch_size=args.batch_size, num_workers=args.num_workers, shuffle=False, pin_memory=True)
 
-            train_dataset = PathDataset(trn_img_paths, trn_labels, default_transforms, train_transforms)
-            valid_dataset = PathDataset(trn_img_paths, trn_labels, default_transforms, valid_transforms)
-            train_loader = DataLoader(dataset=train_dataset, batch_size=args.batch_size, num_workers=args.num_workers, shuffle=True, pin_memory=True)
-            valid_loader = DataLoader(dataset=valid_dataset, batch_size=args.batch_size, num_workers=args.num_workers, shuffle=False, pin_memory=True)
+        # define model
+        model = build_model(args, device)
 
-            # define model
-            model = build_model(args, device)
+        # optimizer definition
+        optimizer = build_optimizer(args, model)
+        scheduler = build_scheduler(args, optimizer, len(train_loader))
+        criterion = nn.CrossEntropyLoss()
 
-            # optimizer definition
-            optimizer = build_optimizer(args, model)
-            scheduler = build_scheduler(args, optimizer, len(train_loader))
-            criterion = nn.BCELoss()
+        trn_cfg = {'train_loader':train_loader,
+                    'valid_loader':valid_loader,
+                    'model':model,
+                    'criterion':criterion,
+                    'optimizer':optimizer,
+                    'scheduler':scheduler,
+                    'device':device,
+                    }
 
-            trn_cfg = {'train_loader':train_loader,
-                       'valid_loader':valid_loader,
-                       'model':model,
-                       'criterion':criterion,
-                       'optimizer':optimizer,
-                       'scheduler':scheduler,
-                       'device':device,
-                       }
+        train(args, trn_cfg)
 
-            train(args, trn_cfg)
-
-            del model, train_loader, valid_loader, train_dataset, valid_dataset
-            gc.collect()
-               
+        del model, train_loader, valid_loader, train_dataset, valid_dataset
+        gc.collect()
+            
 
 if __name__ == '__main__':
-
-    ########## ENVIRONMENT SETUP ############
-    parser = argparse.ArgumentParser(description='parser')
-    arg = parser.add_argument
-
-    # custom args
-    arg('--SEED', type=int, default=42)
-    arg('--n_folds', type=int, default=5, help='number of folds to train')
-    arg('--epochs', type=int, default=20, help='number of epochs to train')
-    arg('--num_classes', type=int, default=1)
-    arg('--input_size', type=int, default=512)
-    arg('--batch_size', type=int, default=8)
-    arg('--num_workers', type=int, default=4)
-    arg('--model', type=str, default='efficientnet_b4')
-    arg('--optimizer', type=str, default='AdamW')
-    arg('--scheduler', type=str, default='Plateau', help='scheduler in steplr, plateau, cosine')
-    arg('--lr', type=float, default=1e-4) 
-    arg('--weight_decay', type=float, default=0.0) 
-    arg('--train_augments', type=str, default='random_crop, horizontal_flip, vertical_flip, random_rotate, random_grayscale')
-    arg('--valid_augments', type=str, default='horizontal_flip, vertical_flip')
-    arg('--augment_ratio', default=0.5, type=float, help='probability of implementing transforms')
-    arg('--pretrained', default=False, type=bool, help='download pretrained model')
-    arg('--lookahead', default=False, type=bool, help='use lookahead')
-    arg('--k_param', type=int, default=5)
-    arg('--alpha_param', type=float, default=0.5)
-    arg('--patience', type=int, default=3, help='plateau scheduler patience parameter')
-    arg('--DEBUG', default=False, type=bool, help='if true debugging mode')
-    args = parser.parse_args()
-
-    main(args)
+    print(args)
+    main()
     