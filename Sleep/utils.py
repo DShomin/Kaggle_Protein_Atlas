c1bb224019a0c66d5f3f7a5f393bb8d8072f30c3
marquis08 <roal214@naver.com>
2021-01-31 20:54:47 +0900

    add cudnn.benchmark

--- Sleep/utils.py
+++ Sleep/utils.py
@@ -15,7 +15,8 @@ def seed_everything(seed):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.deterministic = False
+    torch.backends.cudnn.benchmark = True
 
 
 def find_th(pred, n):
